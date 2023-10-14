594db79a47d1fb5c2ae9aa7186aca5856e137ba3
alex <alexkuf1978@outloock.com>
2023-10-14 12:22:39 +0300

    Fixed contact

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -3,10 +3,33 @@ import emailjs from "@emailjs/browser";
 import { toast } from "react-toastify";
 import { useState } from "react";
 import "react-toastify/dist/ReactToastify.css";
+import Joi from "joi";
+import { formikValidateUsingJoi } from "../utils/formikValidateUsingJoi";
+import { useFormik } from "formik";
+import Input from "./common/input";
+import Textarea from "./common/textarea";
 
 const ContactForm = () => {
-  const [setError] = useState("");
+  const [error, setError] = useState("");
   const form = useRef();
+
+  const form1 = useFormik({
+    validateOnMount: true,
+    initialValues: {
+      user_name: "",
+      user_email: "",
+      message: "",
+    },
+    validate: formikValidateUsingJoi({
+      user_name: Joi.string().min(4).max(255).required(),
+      user_email: Joi.string()
+        .min(6)
+        .max(255)
+        .required()
+        .email({ tlds: { allow: false } }),
+      message: Joi.string().min(6).max(1024).required(),
+    }),
+  });
 
   const sendEmail = (e) => {
     e.preventDefault();
@@ -19,55 +42,63 @@ const ContactForm = () => {
       )
       .then(
         (result) => {
-          if (result.status === 200) {
-            toast.success("Messages sent");
-          }
+          toast.success("Messages sent", result);
+          form1.resetForm();
         },
         (error) => {
           setError(error.text);
         }
       );
-    e.target.reset();
   };
+
   return (
-    <div className="container" style={{ maxWidth: "400px" }}>
-      <h2 className="mb-5 text-center">Contact Me:</h2>
-      <form ref={form} onSubmit={sendEmail}>
-        <div className="col">
-          <div className="col-12 m-auto">
-            <label>Name</label>
-            <input
-              type="text"
-              name="user_name"
-              className="form-control mb-2 text-center"
-              placeholder="Name"
-            ></input>
-          </div>
-          <div className="col-12 m-auto">
-            <label>Email address</label>
-            <input
-              type="email"
-              name="user_email"
-              className="form-control mb-2 text-center"
-              placeholder="Email address"
-            ></input>
-          </div>
-          <div className="col-12 m-auto">
-            <label>Message</label>
-            <textarea
-              rows={3}
-              name="message"
-              className="form-control mb-4 text-center"
-              placeholder="Message"
-            ></textarea>
-          </div>
-          <div className="col-12 m-auto">
-            <div className="row-8">
-              <button className="btn btn-primary mb-5" type="submit">
-                Submit
-              </button>
-            </div>
-          </div>
+    <div className="container">
+      <h2 className="text-center pt-5">Contact me</h2>
+      <form
+        ref={form}
+        onSubmit={sendEmail}
+        noValidate
+        style={{
+          maxWidth: "32rem",
+          justifyContent: "center",
+          margin: "auto",
+          padding: "5px",
+        }}
+      >
+        {error && <div className="alert alert-danger">{error}</div>}
+        <Input
+          {...form1.getFieldProps("user_name")}
+          type="user_name"
+          name="user_name"
+          label="Name"
+          required
+          error={form1.touched.user_name && form1.errors.user_name}
+        />
+        <Input
+          {...form1.getFieldProps("user_email")}
+          type="user_email"
+          name="user_email"
+          label="Email"
+          required
+          error={form1.touched.user_email && form1.errors.user_email}
+        />
+        <Textarea
+          {...form1.getFieldProps("message")}
+          type="message"
+          name="message"
+          label="Message"
+          required
+          error={form1.touched.message && form1.errors.message}
+        />
+
+        <div className="d-grid gap-2 mt-2 pb-5">
+          <button
+            type="submit"
+            className="btn btn-primary mt-2"
+            disabled={!form1.isValid}
+          >
+            Send
+          </button>
         </div>
       </form>
     </div>
